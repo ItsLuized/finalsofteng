8afb704abaebc6a21e06a97a63f08c3a3ff39914
Luis Martinez <luisemartinezcruz@gmail.com>
2020-05-18 17:30:04 -0500

    Project Setup

---
+++ src/main/java/com/softeng/finalsofteng/FinalsoftengApplication.java
@@ -0,0 +1,13 @@
+package com.softeng.finalsofteng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalsoftengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalsoftengApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/softeng/finalsofteng/FinalsoftengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softeng.finalsofteng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalsoftengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
